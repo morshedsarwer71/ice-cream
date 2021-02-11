f66566fe9f4210d29be99c971a26762a020f0f23
Morshed Sarwer <morshedsarwer@gmail.com>
2021-02-12 03:40:56 +0600

    error commit

---
+++ src/Components/Body/Body.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Body.module.css';
+const Body = () => {
+    return (
+        <div className={classes.mainBody}>
+            body
+        </div>
+        
+    )
+}
+
+export default Body
